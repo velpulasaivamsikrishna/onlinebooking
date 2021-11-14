23a35a94dab54bc48de063e521f8a7e1575cfa4c
Sai Vamsi Krishna <velpulasaivamsikrishna@gmail.com>
2021-11-14 19:49:33 +0530

    message

---
+++ src/test/java/com/example/onlinebooking/OnlineBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
